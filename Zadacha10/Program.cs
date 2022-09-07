82ae327804e83ff77f562e067bf80f0a6ee3f396
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-07 13:55:38 +0500

    add Zadacha 10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите целое трёхзначное число: ");
+
+while(true)
+{
+    string num3str = Console.ReadLine();
+    int lengthString = num3str.Length;
+    string pattern = @"^\d+$";
+    if (lengthString == 3 && Regex.IsMatch(num3str, pattern))
+    {   
+        int num3 = ((Int32.Parse(num3str)) / 10) % 10;
+        Console.WriteLine(num3);
+        break;
+    }
+    else
+    {
+        if (num3str.ToLower() != "q")
+        {
+            Console.Write("Ошибка! Введите целое трёхзначное число: ");
+        }
+        else
+        {
+            Console.WriteLine("Выход из программы");
+            break;
+        }
+        
+    }
+};
+
+
