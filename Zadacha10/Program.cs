35412374560a68534805252ef7863fbbe768e2fc
Roman Magafurov <roman.mag1997@yandex.ru>
2022-09-08 16:46:54 +0500

    Update Zadacha  10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -3,32 +3,45 @@ using System.Text.RegularExpressions;
 
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число
 // и на выходе показывает вторую цифру этого числа.
-Console.Write("Введите целое трёхзначное число: ");
+Console.Write("Чтобы выйти из программы, введите символ \"q\" в терминале.\nВведите целое трёхзначное число: ");
 
-while(true)
+while(true) // Всегда работает
 {
-    string num3str = Console.ReadLine();
-    int lengthString = num3str.Length;
-    string pattern = @"^\d+$";
-    if (lengthString == 3 && Regex.IsMatch(num3str, pattern))
-    {   
-        int num3 = ((Int32.Parse(num3str)) / 10) % 10;
-        Console.WriteLine(num3);
-        break;
-    }
-    else
+    try // Обработка исключения
     {
-        if (num3str.ToLower() != "q")
+        string num3str = Console.ReadLine(); // Присваивает переменную из консоли
+        string pattern = @"^\d+$"; // Regex, на наличие целых чисел
+        if (Regex.IsMatch(num3str, pattern)) // Если в строке есть цифры, то переходи на следующий уровень
         {
-            Console.Write("Ошибка! Введите целое трёхзначное число: ");
+            if (int.Parse(num3str).ToString().Length == 3 && Regex.IsMatch(num3str, pattern)) // Плюс, если длина строки равна 3, то, переходи дальше
+            {   
+                int num3 = (Convert.ToInt32(int.Parse(num3str).ToString()[2].ToString())); // Конвертация str num3str в int, и присваивает переменную num3
+                Console.Write($"Чтобы выйти из программы, введите символ \"q\" в терминале.\nВведёное число {num3}\n"
+                                +"Введите целое трёхзначное число: ");
+            }
+            else // Иначе длина строки не равна 3
+            {
+                Console.Write($"Чтобы выйти из программы, введите символ \"q\" в терминале.\nВведёное число {int.Parse(num3str).ToString()} не трёхзначное.\n"
+                             +"Введите целое трёхзначное число: ");
+            }
         }
-        else
+        else // Иначе в строке присутствует кроме цифр ещё символ, который не относится к цифрам
         {
-            Console.WriteLine("Выход из программы");
-            break;
+                if (num3str.ToLower() == "q" || num3str.ToLower() == "й") // Если в строке присутствует символ "q" или "й", то переходи вниз
+                {
+                    Console.WriteLine("Выход из программы");
+                    break;
+                }
+                else // Иначе
+                {
+                    Console.Write("Чтобы выйти из программы, введите символ \"q\" в терминале.\nЭто не число! Введите целое трёхзначное число: ");
+                }
         }
-        
     }
+    catch(System.OverflowException) // Если показана ошибка OverflowException, тогда, зафиксируй её, и работай дальше.
+    {
+        Console.Write("Ошибка. Значение было либо слишком большим, либо слишком маленьким.\n"
+                     +"Чтобы выйти из программы, введите символ \"q\" в терминале.\n"
+                     +"Введите целое трёхзначное число: ");
+    }    
 };
-
-
